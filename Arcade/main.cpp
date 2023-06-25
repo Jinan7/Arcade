ad1a521462f0fa919afcc8b029f9f8bd54e277bd
Jinan Lakan <lakanj25@gmail.com>
2023-06-25 23:29:12 +0200

    create game window:

--- Arcade/main.cpp
+++ Arcade/main.cpp
@@ -3,11 +3,37 @@
 #include "Vec2D.h"
 using namespace std;
 
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
 int main(int argc, char* []) {
-	Vec2D V(3, 7);
-	Vec2D result = 3 * V;
+	
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "error" << endl;
+		return 1;
+	}
 
-	cout << V << endl;
-	cout << result << endl;
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+	if (optrWindow == nullptr) {
+		std::cout << "Could not create window " << SDL_GetError() << endl;
+	}
+
+	
+	SDL_Event sdlEvent;
+	bool running = true;
+	
+	while (running) {
+		while (SDL_PollEvent(&sdlEvent)) {
+			switch (sdlEvent.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+	
+
+	SDL_DestroyWindow(optrWindow);
+	SDL_Quit();
 	return 0;
 }
