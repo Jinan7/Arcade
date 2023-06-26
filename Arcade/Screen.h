84d68da52bc9653f97426e800a13a8da2a415983
Jinan Lakan <lakanj25@gmail.com>
2023-06-26 21:01:03 +0200

    Add polyfill method

--- Arcade/Screen.h
+++ Arcade/Screen.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include "ScreenBuffer.h"
 #include "Color.h"
+#include <vector>
 
 class Line2D;
 class Vec2D;
@@ -30,13 +31,14 @@ class Screen
 		void Draw(int x, int y, const Color& color);
 		void Draw(const Vec2D& point, const Color& color);
 		void Draw(const Line2D& line, const Color& color);
-		void Draw(const Triangle& triangle, const Color& color);
-		void Draw(const AARectangle& rect, const Color& color);
-		void Draw(const Circle& circle, const Color& color);
+		void Draw(const Triangle& triangle, const Color& color, bool fill = false, const Color& fillColor = Color::White());
+		void Draw(const AARectangle& rect, const Color& color, bool fill = false, const Color& fillColor = Color::White());
+		void Draw(const Circle& circle, const Color& color, bool fill = false, const Color& fillColor = Color::White());
 	private:
 		Screen(const Screen& screen);
 		Screen& operator=(const Screen& screen);
 		void ClearScreen();
+		void FillPoly(const std::vector<Vec2D>& points, const Color& color);
 		uint32_t mWidth;
 		uint32_t mHeight;
 
