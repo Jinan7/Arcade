a4e267430b54ed23aeac6fd099051a234c0c93e7
Jinan Lakan <lakanj25@gmail.com>
2023-06-26 22:56:51 +0200

    Add timer

--- Arcade/App.cpp
+++ Arcade/App.cpp
@@ -26,14 +26,25 @@ void App::Run() {
 		Triangle triangle = { Vec2D(60, 10), Vec2D(10, 110), Vec2D(110,110) };
 		Circle circle = { Vec2D(mScreen.Width() / 2 + 50, mScreen.Height() / 2 + 50), 50 };
 
-		mScreen.Draw(triangle, Color::White(), true, Color::Red());
-		mScreen.Draw(circle, Color::Blue(), true, Color::Red());
-		mScreen.SwapScreen();
 
 		SDL_Event sdlEvent;
 		bool running = true;
 
+		uint32_t lastTick = SDL_GetTicks();
+		uint32_t currentTick = lastTick;
+		uint32_t dt = 10;
+		uint32_t accumulator = 0;
+
 		while (running) {
+			currentTick = SDL_GetTicks();
+			uint32_t frameTime = currentTick - lastTick;
+			if (frameTime > 300)
+			{
+				frameTime = 300;
+			}
+			lastTick = currentTick;
+			accumulator += frameTime;
+
 			while (SDL_PollEvent(&sdlEvent)) {
 				switch (sdlEvent.type) {
 				case SDL_QUIT:
@@ -41,6 +52,14 @@ void App::Run() {
 					break;
 				}
 			}
+			while (accumulator >= dt) {
+				std::cout << "delta time " << dt << std::endl;
+				accumulator -= dt;
+			}
+
+			mScreen.Draw(triangle, Color::White(), true, Color::Red());
+			mScreen.Draw(circle, Color::Blue(), true, Color::Red());
+			mScreen.SwapScreen();
 		}
 
 	}
