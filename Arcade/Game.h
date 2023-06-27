ee22164511844d844a97f4dcdd8e9bd54b46fa81
Jinan Lakan <lakanj25@gmail.com>
2023-06-27 04:24:44 +0200

    Create the game interface

---
+++ Arcade/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef GAME_H_
+#define GAME_H_
+#include <stdint.h>
+#include <string>
+
+class GameController;
+class Screen;
+
+class Game
+{
+public:
+	virtual ~Game(){}
+	virtual void Update(uint32_t dt) = 0;
+	virtual void Draw(Screen& screen) = 0;
+	virtual std::string GetName() const = 0;
+};
+
+#endif // !GAME_H_
+
+
+
